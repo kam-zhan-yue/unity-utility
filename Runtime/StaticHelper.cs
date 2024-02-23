05caece036cc206c277be2e3afe431ce5632ba7e
kamzhanyue <kamzhanyue@gmail.com>
2024-02-24 02:38:54 +1100

    Add Set Layer Recursively

--- Runtime/StaticHelper.cs
+++ Runtime/StaticHelper.cs
@@ -12,97 +12,106 @@ namespace Kuroneko.UtilityDelivery
         
     private const float FPS = 24;
 
-    public static void AddOnce<T>(this HashSet<T> _list, T _item)
+    public static void AddOnce<T>(this HashSet<T> list, T item)
     {
-        if (_item != null && !_list.Contains(_item))
-            _list.Add(_item);
+        if (item != null && !list.Contains(item))
+            list.Add(item);
     }
     
-    public static void AddOnce<T>(this List<T> _list, T _item)
+    public static void AddOnce<T>(this List<T> list, T item)
     {
-        if (_item != null && !_list.Contains(_item))
-            _list.Add(_item);
+        if (item != null && !list.Contains(item))
+            list.Add(item);
     }
     
-    public static void SetActiveFast(this GameObject _gameObject, bool _active)
+    public static void SetActiveFast(this GameObject gameObject, bool active)
     {
-        if (_gameObject.activeSelf == _active)
+        if (gameObject.activeSelf == active)
             return;
 
-        _gameObject.SetActive(_active);
+        gameObject.SetActive(active);
     }
 
-    public static bool BelongsToLayerMask(this GameObject _gameObject, LayerMask _layerMask)
+    public static bool BelongsToLayerMask(this GameObject gameObject, LayerMask layerMask)
     {
-        return ((1 << _gameObject.layer) & _layerMask) != 0;
+        return ((1 << gameObject.layer) & layerMask) != 0;
     }
 
-    public static T[] Copy<T>(this List<T> _list)
+    public static T[] Copy<T>(this List<T> list)
     {
-        if (_list == null)
+        if (list == null)
             return Array.Empty<T>();
-        int count = _list.Count;
+        int count = list.Count;
         T[] array = new T[count];
         for (int i = 0; i < count; ++i)
-            array[i] = _list[i];
+            array[i] = list[i];
         return array;
     }
 
-    public static float GetFrameInSeconds(int _frame)
+    public static float GetFrameInSeconds(int frame)
     {
         float frameSecond = 1 / FPS;
-        return _frame * frameSecond;
+        return frame * frameSecond;
     }
     
-    public static float GetFrameInSeconds(int _frame, float _sampleRate)
+    public static float GetFrameInSeconds(int frame, float sampleRate)
     {
-        if (_sampleRate == 0)
+        if (sampleRate == 0)
             return 0;
-        float frameSecond = 1 / _sampleRate;
-        return _frame * frameSecond;
+        float frameSecond = 1 / sampleRate;
+        return frame * frameSecond;
     }
 
-    public static bool BelongsToLayerMask(int _layer, int _layerMask)
+    public static void SetLayerRecursively(this GameObject gameObject, int layerMask)
     {
-        return (_layerMask & (1 << _layer)) > 0;
+        gameObject.layer = layerMask;
+        foreach (Transform child in gameObject.transform)
+        {
+            SetLayerRecursively(child.gameObject, layerMask);
+        }
     }
 
-    public static Vector3 DirectionToObject(this Transform _transformA, Transform _transformB)
+    public static bool BelongsToLayerMask(int layer, int layerMask)
     {
-        return (_transformB.position - _transformA.position).normalized;
+        return (layerMask & (1 << layer)) > 0;
+    }
+
+    public static Vector3 DirectionToObject(this Transform transformA, Transform transformB)
+    {
+        return (transformB.position - transformA.position).normalized;
     }
     
-    public static Vector3 DirectionToPoint(this Transform _transform, Vector3 _point)
+    public static Vector3 DirectionToPoint(this Transform transform, Vector3 point)
     {
-        return (_point - _transform.position).normalized;
+        return (point - transform.position).normalized;
     }
     
-    public static float DistanceToObject(this Transform _transformA, Transform _transformB)
+    public static float DistanceToObject(this Transform transformA, Transform transformB)
     {
-        float distance = Vector3.Distance(_transformA.position, _transformB.position);
+        float distance = Vector3.Distance(transformA.position, transformB.position);
         return distance;
     }
     
-    public static float DistanceToPoint(this Transform _transform, Vector3 _point)
+    public static float DistanceToPoint(this Transform transform, Vector3 point)
     {
-        float distance = Vector3.Distance(_transform.position, _point);
+        float distance = Vector3.Distance(transform.position, point);
         return distance;
     }
 
     /// <summary>
     /// Draws a gizmo rectangle
     /// </summary>
-    /// <param name="_center">Center.</param>
-    /// <param name="_size">Size.</param>
-    /// <param name="_color">Color.</param>
-    public static void DrawGizmoRectangle(Vector2 _center, Vector2 _size, Color _color)
+    /// <param name="center">Center.</param>
+    /// <param name="size">Size.</param>
+    /// <param name="color">Color.</param>
+    public static void DrawGizmoRectangle(Vector2 center, Vector2 size, Color color)
     {
-        Gizmos.color = _color;
+        Gizmos.color = color;
 
-        Vector3 v3TopLeft = new(_center.x - _size.x/2, _center.y + _size.y/2, 0);
-        Vector3 v3TopRight = new(_center.x + _size.x/2, _center.y + _size.y/2, 0);
-        Vector3 v3BottomRight = new(_center.x + _size.x/2, _center.y - _size.y/2, 0);
-        Vector3 v3BottomLeft = new(_center.x - _size.x/2, _center.y - _size.y/2, 0);
+        Vector3 v3TopLeft = new(center.x - size.x/2, center.y + size.y/2, 0);
+        Vector3 v3TopRight = new(center.x + size.x/2, center.y + size.y/2, 0);
+        Vector3 v3BottomRight = new(center.x + size.x/2, center.y - size.y/2, 0);
+        Vector3 v3BottomLeft = new(center.x - size.x/2, center.y - size.y/2, 0);
 
         Gizmos.DrawLine(v3TopLeft,v3TopRight);
         Gizmos.DrawLine(v3TopRight,v3BottomRight);
@@ -114,19 +123,19 @@ namespace Kuroneko.UtilityDelivery
     /// Draws a debug ray in 2D and does the actual raycast
     /// </summary>
     /// <returns>The raycast hit.</returns>
-    /// <param name="_rayOriginPoint">Ray origin point.</param>
-    /// <param name="_rayDirection">Ray direction.</param>
-    /// <param name="_rayDistance">Ray distance.</param>
-    /// <param name="_mask">Mask.</param>
-    /// <param name="_color">Color.</param>
-    /// <param name="_drawGizmo">Draw gizmo or not</param>
-    public static RaycastHit2D RayCast(Vector2 _rayOriginPoint, Vector2 _rayDirection, float _rayDistance, LayerMask _mask, Color _color,bool _drawGizmo=false)
+    /// <param name="rayOriginPoint">Ray origin point.</param>
+    /// <param name="rayDirection">Ray direction.</param>
+    /// <param name="rayDistance">Ray distance.</param>
+    /// <param name="mask">Mask.</param>
+    /// <param name="color">Color.</param>
+    /// <param name="drawGizmo">Draw gizmo or not</param>
+    public static RaycastHit2D RayCast(Vector2 rayOriginPoint, Vector2 rayDirection, float rayDistance, LayerMask mask, Color color,bool drawGizmo=false)
     {	
-        if (_drawGizmo) 
+        if (drawGizmo) 
         {
-            Debug.DrawRay (_rayOriginPoint, _rayDirection * _rayDistance, _color);
+            Debug.DrawRay (rayOriginPoint, rayDirection * rayDistance, color);
         }
-        return Physics2D.Raycast(_rayOriginPoint,_rayDirection,_rayDistance,_mask);		
+        return Physics2D.Raycast(rayOriginPoint,rayDirection,rayDistance,mask);		
     }
         public static int GetLayerMaskByName(string _layerName)
         {
